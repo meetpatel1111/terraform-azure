cdff967553b2fdd95c93313cc69cc51682e8fc9b
Meetkumar Patel <pmeet464@gmail.com>
2025-11-04 17:38:46 +0530

    Added remote backend support

--- backend.tf
+++ backend.tf
@@ -1,9 +1,3 @@
-# Configure a remote backend if desired. Defaults to local state.
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "tfstate-rg"
-#     storage_account_name = "tfstateacct1234"
-#     container_name       = "tfstate"
-#     key                  = "terraform-azure-infra.tfstate"
-#   }
-# }
+terraform {
+  backend "azurerm" {}
+}
